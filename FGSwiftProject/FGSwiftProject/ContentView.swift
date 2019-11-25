8981db06b5ce718cbf1ed764a2ee47b3f4db99b6
鑫龙魂 <liyaqing_it@126.com>
2019-11-25 22:55:27 +0800

    1.上传工程

---
+++ FGSwiftProject/FGSwiftProject/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  FGSwiftProject
+//
+//  Created by 鑫龙魂 on 2019/11/25.
+//  Copyright © 2019 鑫龙魂. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
